0ce66d373d968a93d27f1aaa120e8cdabd4362d7
Aniket Sunil Badadhe <94600123+AniketBadadhe0570@users.noreply.github.com>
2022-07-01 16:21:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+import React, { Component } from 'react'
+import Container from './components/Container'
+
+
+
+export default class App extends Component {
+  
+  render() {
+    return (<div style={{backgroundColor:'dimgray'}}>
+      <Container/>
+      </div>
+      
+    )
+  }
+}
+
+
+
+
+
